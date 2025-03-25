d526241c54cd41a784fec53decf7bc941050b8b9
Gunnar Torfi <gunnartorfis@gmail.com>
2025-03-25 08:16:47 +0000

    chore: fix english

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -95,7 +95,7 @@ export default function CartModal() {
                 <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
                   <ShoppingCartIcon className="h-16" />
                   <p className="mt-6 text-center text-2xl font-bold">
-                    Your cart is empty.
+                    Karfan þín er tóm.
                   </p>
                 </div>
               ) : (
